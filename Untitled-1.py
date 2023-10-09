858ecb69e8878e23ecd7b4ccedec2f18f37f5fed
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2023-10-09 16:12:45 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,42 @@
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+iris = fetch_ucirepo(id=53) 
+  
+# data (as pandas dataframes) 
+X = iris.data.features 
+y = iris.data.targets 
+  
+# metadata 
+print(iris.metadata) 
+  
+# variable information 
+print(iris.variables) 
+
+
+class Tree:
+    def __init__(self, data):
+        self.children = []
+        self.connect = None
+        self.data = data
+    def PrintTree(self):
+        print(self.data)
+        for i in self.children:
+            i.PrintTree()
+    def PrintConnect(self):
+        print(self.data)
+        if self.connect != None:
+            self.connect.PrintConnect()
+
+
+left = Tree("left")
+middle = Tree("middle")
+right = Tree("right")
+root = Tree("root")
+root.children = [left, middle, right]
+root.data = "root"
+left.data = "left"
+right.data = "right"
+middle.data = "middle"
+middle.connect = right
+middle.PrintConnect()
