70d77e9e360de8f694762709d0152cedc6db992c
tomplo18 <112023770+tomplo18@users.noreply.github.com>
2023-10-20 12:01:24 -0400

    Update main.py
    added WIP findFreqSets function along with helper function; fixed makepointers bug

--- main.py
+++ main.py
@@ -133,10 +133,10 @@ def makePointers(start, dataTree, itemCounts):
                 out.extend(out2)
     return out
 
-def makePointerList(itemCounts, dataTree):
+def makePointerList(itemCounts, dataTree, threshold):
     pointerList = []
     for index, count in itemCounts.items():
-        if count > 500: #threshold value
+        if count >= threshold: #threshold value
             temp = Tree(index)
             listOfItemNodes = makePointers(temp, dataTree, itemCounts)
             temp.same = listOfItemNodes[0]
@@ -145,6 +145,69 @@ def makePointerList(itemCounts, dataTree):
             pointerList.append(temp)
     return pointerList
 
+def findFreqSets(pointerList, threshold):
+    freqItems = []
+    for i in reversed(pointerList):
+        temp = i
+        totalBranch = []
+        while temp.same != None:
+            temp = temp.same
+            countVal = temp.count
+            temp2 = temp.connect
+            curBranch = []
+            while temp2.data != "root":
+                temp2.count = countVal
+                curBranch.append(temp2)
+                temp2 = temp2.connect
+            curBranch.reverse()
+            totalBranch.append(curBranch)
+        if(len(totalBranch) == 1):
+            totalBranch[0].append(i)
+            freqItems.append(totalBranch[0])
+        elif(len(totalBranch) > 1):
+            tempTreeData = genTreeData(totalBranch)
+            tempTree = gentree(tempTreeData)
+            tempStockcode = makeTempSeries(totalBranch)
+            print(tempStockcode)
+            tempPointers = makePointerList(tempStockcode, tempTree, threshold)
+            tempItems = findFreqSets(tempPointers, threshold)
+            for j in tempItems: #CHECK THIS PART
+                j.append(temp)
+            freqItems.extend(tempItems)
+    print(freqItems)
+    return freqItems
+
+def genTreeData(branchList):
+    output = []
+    for i in branchList:
+        curList = []
+        for item in i:
+            curList.append(item.data)
+        output.append(curList)
+    return output
+
+def makeTempSeries(data_count_list):
+    # Create a dictionary to store the counts for each unique "data" value
+    counts_dict = {}
+    
+    # Iterate through the list of DataCount objects and update the counts
+    for i in data_count_list:
+        for item in i:
+            data = item.data
+            count = item.count
+            if data in counts_dict:
+                counts_dict[data] += count
+            else:
+                counts_dict[data] = count
+    
+    # Create a Pandas Series from the counts dictionary
+    counts_series = pd.Series(counts_dict)
+    
+    return counts_series
+
+
+threshold = 2
+'''
 n = 'C:/Class Folders/ESE327/mainProject/filtered_excel_file.xlsx'
 co= ['Invoice', 'StockCode']
 df= extractex(n, co)
@@ -163,7 +226,8 @@ ordered_transaction_list = MakeTransactionList(transaction_data['Invoice'], tran
 
 dataTree = gentree(ordered_transaction_list)
 
-pointerList = makePointerList(stockcode_counts, dataTree)
+pointerList = makePointerList(stockcode_counts, dataTree, threshold)
+'''
 
 '''
 data1= ['1','2','5',], ['3','4'],['1','3'], ['1','2']
@@ -174,7 +238,19 @@ stockData1 = {'1': 3, '2': 2, '3': 2, '4': 1, '5': 1}
 ser = pd.Series(data=stockData1, index=['1', '2', '3', '4', '5'])
 #print(ser)
 
-pointerList = makePointerList(ser, dataTree1)
-'''
+pointerList = makePointerList(ser, dataTree1, threshold)
+a = findFreqSets(pointerList, threshold)
 print("done")
+'''
 
+#textbook example
+data1= [['2','1','5'], ['2','4'],['2','3'], ['2','1', '4'], ['1', '3'], ['2','3'], ['1', '3'], ['2','1', '3', '5'], ['2','1', '3',]]    
+dataTree1 = gentree(data1)
+#dataTree1.PrintTree()
+stockData1 = {'2': 7, '1': 6, '3': 6, '4': 2, '5': 2}
+ser = pd.Series(data=stockData1, index=['1', '2', '3', '4', '5'])
+#print(ser)
+
+pointerList = makePointerList(ser, dataTree1, threshold)
+a = findFreqSets(pointerList, threshold)
+print(a)
